90a5684d236acf402fdcab8f1a04a61962fb96e7
connorruss2005-cmd <connor.russ2005@gmail.com>
2026-07-24 15:38:50 +1200

    Added more text

--- test_example.py
+++ test_example.py
@@ -1,4 +1,4 @@
 #this is a test file
 #Introduction
 #Hi, I'm Kazushi. I'm from Fukushima, where is located in northeastern Japan. I'd be happy to get along with you.
-#Hello! I'm Connor :).
+#Hello! I'm Connor :). Matplotlib, numpy, and pandas are my favorite libraries. 
