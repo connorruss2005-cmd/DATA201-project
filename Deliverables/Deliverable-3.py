8dad28e4eedb7831f5af74dece11545ab8e85d89
connorruss2005-cmd <connor.russ2005@gmail.com>
2026-08-10 12:37:05 +1200

    modified again.

--- Deliverables/Deliverable-3.py
+++ Deliverables/Deliverable-3.py
@@ -1,14 +1,13 @@
 # Packages we are using, please install them if you don't have them yet.
 # Also be sure you have all the listings.csv files downloaded.
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt # Not sure if we need this
 import pandas as pd
 
 df = pd.read_csv('listings.csv')
-
 pd.set_option('display.max_columns', None)
 
-# Convert last_review to datetime
+# Convert last_review to datetime format
 df['last_review'] = pd.to_datetime(df['last_review'])
 
 # Create month + year column
