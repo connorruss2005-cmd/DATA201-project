93ebab8e966714bca5ddc8fb0456e810997c261d
connorruss2005-cmd <connor.russ2005@gmail.com>
2026-08-10 12:27:25 +1200

    Starting working on Deliverable 3. This deliverable will focus on analyzing the data and providing insights based on the listings, reviews, and neighbourhoods datasets.

--- Deliverables/Deliverable-3.py
+++ Deliverables/Deliverable-3.py
@@ -1,9 +1,20 @@
 # Packages we are using, please install them if you don't have them yet.
 # Also be sure you have all the listings.csv files downloaded.
-import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
-listings_df = pd.read_csv('listings.csv')
+df = pd.read_csv('listings.csv')
 
-print(listings_df.head())
+pd.set_option('display.max_columns', None)
+
+# Convert last_review to datetime
+df['last_review'] = pd.to_datetime(df['last_review'])
+
+# Create month + year column
+df['month/year'] = df['last_review'].dt.strftime('%B %Y')
+
+# Save updated csv
+df.to_csv('listings.csv', index=False)
+
+print(df.head())
