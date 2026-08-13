26f4c435313385bb2720a87696c6a01bb9a230dd
connorruss2005-cmd <connor.russ2005@gmail.com>
2026-08-13 15:47:48 +1200

    Changed a lot of stuff

--- Deliverables/Deliverable-3.py
+++ Deliverables/Deliverable-3.py
@@ -4,16 +4,34 @@ import numpy as np
 import matplotlib.pyplot as plt # Not sure if we need this
 import pandas as pd
 
-df = pd.read_csv('listings.csv')
-pd.set_option('display.max_columns', None)
+# Iterate through the 9 listings files
+#for i in range(1, 10):
+#    filename = f"listings{i}.csv"
+#
+#    df = pd.read_csv(filename)
+#
+#    # Filter for Christchurch City listings
+#    df = df[df["neighbourhood_group"] == "Christchurch City"]
+#
+#    # Convert the last_review column to datetime format
+#    df["last_review"] = pd.to_datetime(df["last_review"])
+#    
+#    # Add a new column for month/year of the last review
+#    df["month/year"] = df["last_review"].dt.to_period("M")
+#
+#    df.to_csv(f"listings_{i}_updated.csv", index=False)
 
-# Convert last_review to datetime format
-df['last_review'] = pd.to_datetime(df['last_review'])
 
-# Create month + year column
-df['month/year'] = df['last_review'].dt.strftime('%B %Y')
+data_sets = []
 
-# Saves updated csv
-df.to_csv('listings.csv', index=False)
+for i in range(1, 10):
+    filename = f"listings_{i}_updated.csv"
+    df = pd.read_csv(filename)
 
-print(df.head())
+    # Add the dataframes to an empty list
+    data_sets.append(df)
+
+# Concatenate all datasets
+combined = pd.concat(data_sets, ignore_index=True)
+
+combined.to_csv("combined_Christchurch_listings.csv", index=False)
