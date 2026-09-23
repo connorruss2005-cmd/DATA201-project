9c93af13f261b470fd1a20ddb01789b96d602734
connorruss2005-cmd <connor.russ2005@gmail.com>
2026-09-23 14:25:55 +1200

    new file

---
+++ Deliverables/Deliverable-5/Dataset_join.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+# ===== 1. Load modified Airbnb data and rental bond data =====
+
+df_Airbnb = pd.read_csv("MODIFIED AIRBNB DATA GOES HERE")
+df_Bond = pd.read_csv("RENTAL BOND DATA GOES HERE")
+
+df_Airbnb.head()
+df_Bond.head()
+
